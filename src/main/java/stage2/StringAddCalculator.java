eeb5e30a7473ea70dfe466c5fda490c9a6e732cd
doxxx <doxxx93@gmail.com>
2023-08-07 11:56:04 +0900

    feat: StringAddCalculator skeleton code and related tests

---
+++ src/main/java/stage2/StringAddCalculator.java
@@ -0,0 +1,8 @@
+package stage2;
+
+public class StringAddCalculator {
+
+    public static int splitAndSum(String input) {
+        throw new UnsupportedOperationException("Unsupported splitAndSum");
+    }
+}
