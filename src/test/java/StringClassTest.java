6e540a9e1e512b1560b69414dfa60c0461e2fc24
doxxx <doxxx93@gmail.com>
2023-08-07 10:56:30 +0900

    refactor: clean up the StringClassTest methods

--- src/test/java/StringClassTest.java
+++ src/test/java/StringClassTest.java
@@ -7,7 +7,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringClassTest {
 
-    public static final String COMMA = ",";
+
+    private static final String COMMA = ",";
+    private static final String STRING_ABC = "abc";
 
     @Nested
     @DisplayName("요구사항 1")
@@ -15,21 +17,17 @@ public class StringClassTest {
         @Test
         @DisplayName("\"1,2\"를 ,로 split하면 1과 2로 분리된다.")
         void split1() {
-            final String input = "1,2";
-
-            final String[] result = input.split(COMMA);
-
-            assertThat(result).contains("1", "2");
+            assertThat(splitString("1,2")).contains("1", "2");
         }
 
         @Test
         @DisplayName("\"1\"를 ,로 split하면 1만 포함하는 배열이 반환된다.")
         void split2() {
-            final String input = "1";
+            assertThat(splitString("1")).containsExactly("1");
+        }
 
-            final String[] result = input.split(COMMA);
-
-            assertThat(result).containsExactly("1");
+        private static String[] splitString(String input) {
+            return input.split(COMMA);
         }
     }
 
@@ -40,12 +38,13 @@ public class StringClassTest {
         @Test
         @DisplayName("\"(1,2)\"가 주어졌을 때 substring() 메서드를 활용해 ()를 제거하면 \"1,2\"가 반환된다.")
         void substring() {
-            final String input = "(1,2)";
-
-            final String result = input.substring(1, input.length() - 1);
-
-            assertThat(result).isEqualTo("1,2");
+            assertThat(substringWithoutParentheses("(1,2)")).isEqualTo("1,2");
         }
+
+        private static String substringWithoutParentheses(String input) {
+            return input.substring(1, input.length() - 1);
+        }
+
     }
 
     @Nested
@@ -54,23 +53,18 @@ public class StringClassTest {
 
         @Test
         @DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 수 있다.")
-        void chartAt1() {
-            final String input = "abc";
-
-            final char result = input.charAt(1);
-
-            assertThat(result).isEqualTo('b');
+        void charAtWithValidIndex() {
+            assertThat(STRING_ABC.charAt(1)).isEqualTo('b');
         }
 
         @Test
         @DisplayName("String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생한다.")
-        void charAt2() {
-            final String input = "abc";
-
+        void charAtWithInvalidIndex() {
             assertThatThrownBy(
-                    () -> input.charAt(input.length())
+                    () -> STRING_ABC.charAt(STRING_ABC.length())
             ).isInstanceOf(StringIndexOutOfBoundsException.class)
-                    .hasMessageContaining("String index out of range: %d".formatted(input.length()));
+                    .hasMessageContaining("String index out of range: %d"
+                            .formatted(STRING_ABC.length()));
         }
     }
 }
