a43f7f5d67c9912c0746bd81644b336eca4ace38
doxxx <doxxx93@gmail.com>
2023-08-08 12:47:42 +0900

    fix: ResultView

--- src/main/java/stage3/ResultView.java
+++ src/main/java/stage3/ResultView.java
@@ -3,6 +3,7 @@ package stage3;
 public class ResultView {
 
     public static void printResult() {
+        System.out.println();
         System.out.println("실행 결과");
     }
     public static void printCarsState(Cars cars) {
